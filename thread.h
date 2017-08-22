0c3e52655e13295afd593d0d76d4e151edaa4a4b
Vaibhav150123045 <31079441+Vaibhav150123045@users.noreply.github.com>
2017-08-22 09:10:08 +0530

    Update thread.h

--- thread.h
+++ thread.h
@@ -88,6 +88,7 @@ struct thread
     char name[16];                      /* Name (for debugging purposes). */
     uint8_t *stack;                     /* Saved stack pointer. */
     int priority;                       /* Priority. */
+	int prev_priority;
     struct list_elem allelem;           /* List element for all threads list. */
 	struct list_elem waitElem;
 	int64_t waitTime;
